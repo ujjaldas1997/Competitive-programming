18eac299cb90b413d1d130d5f58284e63c8431a0
Ujjal Kumar Das <ujjaldas223@gmail.com>
2017-07-30 01:26:40 +0530

    Add files via upload

---
+++ gen_triplet.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def gen_prim_pyth_trips(limit=None):
+    u = np.mat(' 1  2  2; -2 -1 -2; 2 2 3')
+    a = np.mat(' 1  2  2;  2  1  2; 2 2 3')
+    d = np.mat('-1 -2 -2;  2  1  2; 2 2 3')
+    uad = np.array([u, a, d])
+    m = np.array([3, 4, 5])
+    while m.size:
+        m = m.reshape(-1, 3)
+        if limit:
+            m = m[m[:, 2] <= limit]
+        yield from m
+        m = np.dot(m, uad)
+print(list(gen_prim_pyth_trips(50)))
