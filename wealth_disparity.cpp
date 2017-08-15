43b0b877a86f5f79bc77e6099fba3b95f4e2d06b
Ujjal Kumar Das <ujjaldas223@gmail.com>
2017-08-15 11:10:19 +0530

    Update wealth_disparity.cpp

--- wealth_disparity.cpp
+++ wealth_disparity.cpp
@@ -1,23 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
+int max_diff = INT_MIN;
+void dfs(map<int, vector<int>> &tree, int root, int maxm, int value[]){
+	if(tree[root].empty()){
+		max_diff = max(max_diff, maxm - value[root]);
+		//cout << "Root = " << root << ", maxm = " << maxm << ", max_diff = " << max_diff << endl;
+		return;
+	}
+	//maxm = max(maxm, value[root]);
+	//max_diff = max(max_diff, maxm - value[root]);
+	for(int i = 0; i < tree[root].size(); ++i){
+		int child = tree[root][i];
+		maxm = max(maxm, value[root]);
+		max_diff = max(max_diff, maxm - value[child]);
+		//cout << "Child = " << child << ", maxm = " << maxm << ", max_diff = " << max_diff << endl;
+		dfs(tree, child, maxm, value);
+	}
+}
 int main()
 {
-	map<int, int> id;
-	int n = 0, maxm = -100000000;
+	map<int, vector<int>> tree;
+	int n = 0, root;
+	int maxm = INT_MIN;
 	cin >> n;
-	int arr[n] = {0};
-	for(int i = 0; i < n; ++i){
-		cin >> arr[i];
-		id[i + 1] = arr[i];
-	}
-	for(int i = 0; i < n; ++i){
+	int value[n + 1] = {0};
+	for(int i = 1; i <= n; ++i)
+		cin >> value[i];
+	for(int i = 1; i <= n; ++i){
 		int p;
 		cin >> p;
-		if(p != -1){
-			maxm = max(maxm, id[p] - arr[i]);
-			//cout << "maxm = " << maxm << " id[p] = " << id[p] << " arr[i] = " << arr[i] << endl;
-		}
+		if(p == -1) root = i;
+		else tree[p].push_back(i);
 	}
-	cout << maxm <<endl;
+	/*for(auto it = tree.begin(); it != tree.end(); ++it){
+		cout << "Parent : " << it->first << " Child : ";
+		for(int i = 0; i < it->second.size(); ++i)
+			cout << it->second[i] << " ";
+		cout << endl;
+	}*/
+	maxm = value[root];
+	dfs(tree, root, maxm, value);
+	cout << max_diff << endl;
 	return 0;
 }
